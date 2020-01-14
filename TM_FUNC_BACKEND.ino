984e51f92531964c02b895afe54e133984b50b61
0scarjny <57064478+0scarjny@users.noreply.github.com>
2020-01-14 19:01:07 +0100

    Update TM_FUNC_BACKEND.ino

--- TM_FUNC_BACKEND.ino
+++ TM_FUNC_BACKEND.ino
@@ -45,27 +45,7 @@
     boolean LCDML_BACK_loop(LCDML_BACKEND_RAINBOW)
     {    
     pixels.Update(); 
-    /* if (digitalRead(18) == LOW) // Button #2 pressed
-    {
-        // Switch to alternating color wipes on Rings1 and 2
-        
-        pixels.ActivePattern = CLEARSTRIP;
-        pixels.TotalSteps = pixels.numPixels();
-        LCDML_BACK_stopStable(LCDML_BACKEND_RAINBOW);
-        // And update tbe stick
-        
-    }
-    else // Back to normal operation
-    {
-        // Restore all pattern parameters to normal values
-        
-        pixels.ActivePattern = RAINBOW_CYCLE;
-        pixels.TotalSteps = 255;
-        pixels.Interval = min(10, pixels.Interval);
-        // And update tbe stick
-        
-    }*/
- // g_lcdml_initscreen = millis(); // reset initscreen timer
+    
 }  
     
     
