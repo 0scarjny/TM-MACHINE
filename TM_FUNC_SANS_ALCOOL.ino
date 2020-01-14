6bbc1b0b86658adaf6605a48798fdb09f4771978
0scarjny <57064478+0scarjny@users.noreply.github.com>
2020-01-14 10:57:07 +0100

    Update TM_FUNC_SANS_ALCOOL.ino

--- TM_FUNC_SANS_ALCOOL.ino
+++ TM_FUNC_SANS_ALCOOL.ino
@@ -271,7 +271,7 @@ void LCDML_DISP_loop(LCDML_FUNC_SPRITE)
       distance = (duration / 2) * 0.0343;
       
       }
-      pixels.TheaterChase(pixels.Color(0,1150,130),pixels.Color(0,148,255), 100);
+      pixels.TheaterChase(pixels.Color(0,150,130),pixels.Color(0,148,255), 100);
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print(volume);
@@ -281,7 +281,7 @@ void LCDML_DISP_loop(LCDML_FUNC_SPRITE)
                  
       float verre1=paverage();
       pixels.Update();
-      while(paverage()-verre1 < (volume)*rho_orange && paverage()>poid_minimal){
+      while(paverage()-verre1 < (volume)*rho_sprite && paverage()>poid_minimal){
       
       digitalWrite(PUMP10_IN1, LOW);       
       digitalWrite(PUMP10_IN2, HIGH);
@@ -308,6 +308,161 @@ void LCDML_DISP_loop_end(LCDML_FUNC_SPRITE)
   
   delay(100) ;
   volume = 150;
+  g_lcdml_initscreen = millis(); // reset initscreen timer
+
+  }
+// *********************************************************************
+void LCDML_DISP_setup(LCDML_FUNC_LIME)
+
+// *********************************************************************
+{ 
+      volume =40;
+       
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       lcd.print("Jus de citron");
+       lcd.setCursor(0, 3);
+       lcd.print("Volume:");
+       lcd.setCursor(8, 3);
+       lcd.print(volume);
+       lcd.setCursor(10, 3);
+       lcd.print("ml");
+
+       delay(involontaire);         // pour éviter une sélection involontaire
+  
+}
+void LCDML_DISP_loop(LCDML_FUNC_LIME)
+{
+ menu_choix();
+ 
+  if(LCDML_BUTTON_checkEnter()) {
+      LCDML_BACK_stopStable(LCDML_BACKEND_RAINBOW);
+      duration = sonar.ping();
+      distance = (duration / 2) * 0.0343;
+      // la sortie OUT du capteur est active sur niveau bas
+      while (distance>distance_verre) { // objet détecté = niveau 0 sur OUT capteur
+        
+      verre_manquant();
+      
+      duration = sonar.ping();
+      distance = (duration / 2) * 0.0343;
+      
+      }
+      pixels.TheaterChase(pixels.Color(50,50,130),pixels.Color(70,148,55), 100);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(volume);
+      lcd.setCursor(3, 0);
+      lcd.print("ml de Jus de");
+      lcd.setCursor(0, 1);
+      lcd.print("Citron");
+                 
+      float verre1=paverage();
+      pixels.Update();
+      while(paverage()-verre1 < (volume)*rho_lime && paverage()>poid_minimal){
+      
+      digitalWrite(PUMP12_IN1, LOW);       
+      digitalWrite(PUMP12_IN2, HIGH);
+      Serial.println("Ouvert");
+      pixels.Update();
+    
+      }
+
+      digitalWrite(PUMP12_IN1, LOW);       
+      digitalWrite(PUMP12_IN2, LOW);
+      Serial.println("Fermé1"); 
+           
+      boisson_prete();
+      LCDML_BACK_restart(LCDML_BACKEND_RAINBOW);
+      LCDML.goRoot();
+      }
+      }
+
+void LCDML_DISP_loop_end(LCDML_FUNC_LIME) 
+{
+  // this functions is ever called when a DISP function is quit
+  // you can here reset some global vars or do nothing      
+
+  
+  delay(100) ;
+  volume = 40;
+  g_lcdml_initscreen = millis(); // reset initscreen timer
+
+  }
+// *********************************************************************
+void LCDML_DISP_setup(LCDML_FUNC_GRENADINE)
+
+// *********************************************************************
+{ 
+      volume =40;
+       
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       lcd.print("Sirop de grenadine");
+       lcd.setCursor(0, 3);
+       lcd.print("Volume:");
+       lcd.setCursor(8, 3);
+       lcd.print(volume);
+       lcd.setCursor(10, 3);
+       lcd.print("ml");
+
+       delay(involontaire);         // pour éviter une sélection involontaire
+  
+}
+void LCDML_DISP_loop(LCDML_FUNC_GRENADINE)
+{
+ menu_choix();
+ 
+  if(LCDML_BUTTON_checkEnter()) {
+      LCDML_BACK_stopStable(LCDML_BACKEND_RAINBOW);
+      duration = sonar.ping();
+      distance = (duration / 2) * 0.0343;
+      // la sortie OUT du capteur est active sur niveau bas
+      while (distance>distance_verre) { // objet détecté = niveau 0 sur OUT capteur
+        
+      verre_manquant();
+      
+      duration = sonar.ping();
+      distance = (duration / 2) * 0.0343;
+      
+      }
+      pixels.TheaterChase(pixels.Color(0,150,130),pixels.Color(0,148,255), 100);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(volume);
+      lcd.setCursor(3, 0);
+      lcd.print("ml de Grenadine");
+      
+                 
+      float verre1=paverage();
+      pixels.Update();
+      while(paverage()-verre1 < (volume)*rho_grenadine && paverage()>poid_minimal){
+      
+      digitalWrite(PUMP11_IN1, LOW);       
+      digitalWrite(PUMP11_IN2, HIGH);
+      Serial.println("Ouvert");
+      pixels.Update();
+    
+      }
+
+      digitalWrite(PUMP11_IN1, LOW);       
+      digitalWrite(PUMP11_IN2, LOW);
+      Serial.println("Fermé1"); 
+           
+      boisson_prete();
+      LCDML_BACK_restart(LCDML_BACKEND_RAINBOW);
+      LCDML.goRoot();
+      }
+      }
+
+void LCDML_DISP_loop_end(LCDML_FUNC_GRENADINE) 
+{
+  // this functions is ever called when a DISP function is quit
+  // you can here reset some global vars or do nothing      
+
+  
+  delay(100) ;
+  volume = 40;
   g_lcdml_initscreen = millis(); // reset initscreen timer
 
   }
